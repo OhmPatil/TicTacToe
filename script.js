90658cb5ca63e98fcb0b836b2f3ea40265375836
OhmPatil <workpatil10@gmail.com>
2022-06-30 16:47:28 +0530

    Add player turn changing logic and add markers to DOM

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Global declarations
 let boxes = document.querySelectorAll('#box')
+let box = document.querySelector('#box')
 
 
 // Player Factory function
-const playerFactory = (name, id, marker) => {
-    const speak = function(){
-        return `hi ${name}`
+const playerFactory = (name, id, marker, isActive) => {
+    const changeActiveStatus = function(){
+        this.isActive = !this.isActive
     }
-    return {name, id, marker, speak}
+    return {name, id, marker, isActive, changeActiveStatus}
 }
 
 // Display controller module
@@ -20,10 +21,36 @@ const displayControllerModule = () => {
 
 // Game board module
 const gameBoardModule = () => {
-    const gameboard = []
-    boxes.forEach(box => {
-        gameboard.push(box)
-    })
-    
-    return {gameboard}
+    const gameboard = ['', '', '', '', '', '', '', '', '']
+
+    const renderboard = function(){
+        for (let i = 0; i < 9; i++){
+            boxes[i].textContent = gameboard[i]
+        }
+    }
+    return {gameboard, renderboard}
 }
+
+// Actual game module - All Logic goes here
+const game = () => {
+    let newboard = gameBoardModule()
+    let playerOne = playerFactory('name1', 1, 'X', true)
+    let playerTwo = playerFactory('name2', 2, 'O', false)
+
+    boxes.forEach(box => box.addEventListener('click', function(e){
+        if (!playerOne.isActive) addMark(e.target.dataset.id, playerTwo.marker);
+        else addMark(e.target.dataset.id, playerOne.marker)
+}))
+
+    function addMark(boxID, marker){
+        if (newboard.gameboard[boxID] === ''){
+            newboard.gameboard[boxID] = marker
+            playerOne.changeActiveStatus()
+            playerTwo.changeActiveStatus()
+            newboard.renderboard()
+        }
+    }
+}
+
+newgame = game()
+
