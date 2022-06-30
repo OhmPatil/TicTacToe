3a989d427cb9daa51019c62bd4dbf7e0abc1e9d5
OhmPatil <workpatil10@gmail.com>
2022-06-30 17:48:25 +0530

    Add win and tie logic

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ const playerFactory = (name, id, marker, isActive) => {
 
 // Display controller module
 const displayControllerModule = () => {
-    const displayResult = function(){
-        document.querySelector('.result').textContent = 'X WON'
+    const displayResult = function(string){
+        document.querySelector('.result').textContent = `${string}`
     }
     return {displayResult}
 }
 
 // Game board module
-const gameBoardModule = () => {
+const gameBoardModule = (boardActive) => {
     const gameboard = ['', '', '', '', '', '', '', '', '']
 
     const renderboard = function(){
@@ -28,26 +28,66 @@ const gameBoardModule = () => {
             boxes[i].textContent = gameboard[i]
         }
     }
-    return {gameboard, renderboard}
+
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    const changeBoardActiveStatus = function(){
+        this.boardActive = !this.boardActive
+    }
+    return {gameboard, renderboard, winConditions, changeBoardActiveStatus, boardActive}
 }
 
 // Actual game module - All Logic goes here
 const game = () => {
-    let newboard = gameBoardModule()
-    let playerOne = playerFactory('name1', 1, 'X', true)
-    let playerTwo = playerFactory('name2', 2, 'O', false)
+    let newboard = gameBoardModule(true)
+    let displayController = displayControllerModule()
+    let playerOne = playerFactory('Player1', 1, 'X', true)
+    let playerTwo = playerFactory('Player2', 2, 'O', false)
 
     boxes.forEach(box => box.addEventListener('click', function(e){
         if (!playerOne.isActive) addMark(e.target.dataset.id, playerTwo.marker);
         else addMark(e.target.dataset.id, playerOne.marker)
-}))
+    }))
 
     function addMark(boxID, marker){
-        if (newboard.gameboard[boxID] === ''){
+        if (newboard.gameboard[boxID] === '' && (newboard.boardActive)){
             newboard.gameboard[boxID] = marker
             playerOne.changeActiveStatus()
             playerTwo.changeActiveStatus()
             newboard.renderboard()
+            checkWin()
+        }
+    }
+
+    function checkWin(){
+        for (let i=0; i<=7; i++){
+            let condition = newboard.winConditions[i]
+            const one = newboard.gameboard[condition[0]]
+            const two = newboard.gameboard[condition[1]]
+            const three = newboard.gameboard[condition[2]]
+
+            if (one === '' || two === '' || three === '') continue
+
+            if (one === two && two === three){
+                newboard.changeBoardActiveStatus()
+                if (playerOne.isActive) displayController.displayResult(`${playerTwo.name} WON!`)
+                else displayController.displayResult(`${playerOne.name} WON!`)
+            }
+
+            if (!newboard.gameboard.includes('')){
+                newboard.changeBoardActiveStatus()
+                displayController.displayResult('ITS A TIE')
+            }
+
         }
     }
 }
